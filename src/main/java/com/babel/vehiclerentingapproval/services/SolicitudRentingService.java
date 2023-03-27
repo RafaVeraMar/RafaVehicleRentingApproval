883ac4074d7ed2cece0c0a3e961086021ff7682b
Ramon Vazquez <ramon.vazquez@babelgroup.com>
2023-03-27 14:08:44 +0200

    creados los servicios

---
+++ src/main/java/com/babel/vehiclerentingapproval/services/SolicitudRentingService.java
@@ -0,0 +1,8 @@
+package com.babel.vehiclerentingapproval.services;
+
+import com.babel.vehiclerentingapproval.models.SolicitudRenting;
+
+public interface SolicitudRentingService {
+    public void createRentingRequest(SolicitudRenting solicitudRenting);
+    public void validateRentingRequest(SolicitudRenting solicitudRenting);
+}
---
+++ src/main/java/com/babel/vehiclerentingapproval/services/impl/SolicitudRentingServiceImpl.java
@@ -0,0 +1,24 @@
+package com.babel.vehiclerentingapproval.services.impl;
+
+import com.babel.vehiclerentingapproval.models.SolicitudRenting;
+import com.babel.vehiclerentingapproval.services.SolicitudRentingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SolicitudRentingServiceImpl implements SolicitudRentingService {
+    private SolicitudRentingMapper solicitudRentingMapper;
+
+    public SolicitudRentingServiceImpl(SolicitudRentingMapper solicitudRentingMapper) {
+        this.solicitudRentingMapper = solicitudRentingMapper;
+    }
+
+    @Override
+    public void createRentingRequest(SolicitudRenting solicitudRenting) {
+        
+    }
+
+    @Override
+    public void validateRentingRequest(SolicitudRenting solicitudRenting) {
+
+    }
+}
