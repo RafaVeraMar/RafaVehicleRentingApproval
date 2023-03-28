c329761f41ad5d08b4473259b35942357499b64d
Tomas Prados <tomas.prados@babelgroup.com>
2023-03-28 13:27:52 +0200

    Mapper INSERT Producto Contratado

---
+++ src/main/java/com/babel/vehiclerentingapproval/persistance/database/mappers/ProductoContratadoMapper.java
@@ -0,0 +1,15 @@
+package com.babel.vehiclerentingapproval.persistance.database.mappers;
+
+import com.babel.vehiclerentingapproval.models.ProductoContratado;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+@Mapper
+public interface ProductoContratadoMapper {
+    @Insert("INSERT INTO PRODUCTO_CONTRATADO (PRODUCTO_CONTRATADO_ID, PRODUCTO_ID, ALIAS, IMPORTE_NOMINAL, FECHA_ALTA, FECHA_BAJA)" +
+            " VALUES (#{idProductoContratado}, #{idProducto}, #{alias, jdbcType=VARCHAR}, #{importeNominal}, " +
+            "#{fechaAlta, jdbcType=DATE}, null)")
+    @Options(useGeneratedKeys = true, keyProperty = "idProductoContratado", keyColumn = "PRODUCTO_CONTRATADO_ID")
+    void insertProductoContratado(ProductoContratado productoContratado);
+}
