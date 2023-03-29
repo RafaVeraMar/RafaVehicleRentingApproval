e6289d4a717db085f335e810a309a47471d07251
Alvaro Aleman Rando <alvaro.aleman@babelgroup.com>
2023-03-29 12:39:46 +0200

    Tests hecho para VerSolicitudRenting, si ID no es valido lanza excepcion

--- src/test/java/com/babel/vehiclerentingapproval/Services/impl/SolicitudRentingServiceImplTest.java
+++ src/test/java/com/babel/vehiclerentingapproval/Services/impl/SolicitudRentingServiceImplTest.java
@@ -1,12 +1,14 @@
 package com.babel.vehiclerentingapproval.Services.impl;
 
 import com.babel.vehiclerentingapproval.exceptions.EstadoSolicitudNotFoundException;
+import com.babel.vehiclerentingapproval.exceptions.SolicitudRentingNotFoundException;
 import com.babel.vehiclerentingapproval.persistance.database.mappers.SolicitudRentingMapper;
 import com.babel.vehiclerentingapproval.persistance.database.mappers.TipoResultadoSolicitudMapper;
 import com.babel.vehiclerentingapproval.services.SolicitudRentingService;
 import com.babel.vehiclerentingapproval.services.impl.SolicitudRentingServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -23,11 +25,26 @@ public class SolicitudRentingServiceImplTest {
 
     }
 
-    @Test
-    public void verEstadoSolicitud_should_throwEstadoSolicitudNotFoundException_when_codSolicitudNull_or_idNotExists(){
-        Assertions.assertThrows(EstadoSolicitudNotFoundException.class,() ->{
-            int id = -43;
-            String estado = solicitudService.verEstadoSolicitud(id);
-        });
+    @Nested
+    class TestsVerEstadoSolicitud{
+        @Test
+        public void verEstadoSolicitud_shouldThrow_EstadoSolicitudNotFoundException_when_codSolicitudNull_or_idNotExists(){
+            Assertions.assertThrows(EstadoSolicitudNotFoundException.class,() ->{
+                int id = -43;
+                String estado = solicitudService.verEstadoSolicitud(id);
+            });
+        }
     }
+
+    @Nested
+    class TestsVerSolicitudRenting{
+        @Test
+        public void verSolicitudRenting_shouldThrow_SolicitudRentingNotFoundException_when_solicitudIdNotExists(){
+            Assertions.assertThrows(SolicitudRentingNotFoundException.class,()->{
+                int id = -10;
+                solicitudService.getSolicitudById(id);
+            });
+        }
+    }
+
 }
