f7356e5eb8200b82b75cbcf3fa8b56f8cd9bffee
enriquemunozbabel <125457875+enriquemunozbabel@users.noreply.github.com>
2023-03-29 13:22:34 +0200

    TEST PERSONA

--- src/main/java/com/babel/vehiclerentingapproval/controllers/PersonaController.java
+++ src/main/java/com/babel/vehiclerentingapproval/controllers/PersonaController.java
@@ -1,7 +1,6 @@
 package com.babel.vehiclerentingapproval.controllers;
 
 import com.babel.vehiclerentingapproval.exceptions.RequiredMissingFieldException;
-import com.babel.vehiclerentingapproval.exceptions.WrongLenghtFieldException;
 import com.babel.vehiclerentingapproval.models.Persona;
 import com.babel.vehiclerentingapproval.services.PersonaService;
 import org.apache.coyote.Response;
@@ -21,10 +20,16 @@ public class PersonaController {
     }
 
     @PostMapping("/persona")
-    ResponseEntity addPersona(@RequestBody Persona persona) throws RequiredMissingFieldException, WrongLenghtFieldException {
-        this.personaService.addPersona(persona);
+    ResponseEntity addPersona(@RequestBody Persona persona){
+        try {
+            this.personaService.addPersona(persona);
+        } catch (RequiredMissingFieldException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Comprueba los datos de entrada");
+        }
+        catch (Exception e) {
 
-
-        return ResponseEntity.ok(String.format("Persona añadida. Id: %d", persona.getPersonaId()));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+        return ResponseEntity.ok(persona);
     }
 }
---
+++ src/test/java/com/babel/vehiclerentingapproval/services/impl/PersonaServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.babel.vehiclerentingapproval.services.impl;
+
+import com.babel.vehiclerentingapproval.models.Direccion;
+import com.babel.vehiclerentingapproval.models.Persona;
+import com.babel.vehiclerentingapproval.models.Profesion;
+import com.babel.vehiclerentingapproval.models.Renta;
+import com.babel.vehiclerentingapproval.persistance.database.mappers.*;
+import com.babel.vehiclerentingapproval.services.PersonaService;
+import com.babel.vehiclerentingapproval.services.RentaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+@SpringBootTest
+public class PersonaServiceImplTest {
+    PersonaService personaService;
+    DireccionMapper direccionMapper;
+    PersonaMapper personaMapper;
+    TelefonoMapper telefonoMapper;
+    TipoViaMapper tipoViaMapper;
+    ProvinciaMapper provinciaMapper;
+    PaisMapper paisMapper;
+    ProductoContratadoMapper productoContratadoMapper;
+    @BeforeEach
+    void setUpAll(){
+        personaMapper = Mockito.mock(PersonaMapper.class);
+        direccionMapper = Mockito.mock(DireccionMapper.class);
+        tipoViaMapper = Mockito.mock(TipoViaMapper.class);
+        provinciaMapper = Mockito.mock(ProvinciaMapper.class);
+        telefonoMapper = Mockito.mock(TelefonoMapper.class);
+        paisMapper = Mockito.mock(PaisMapper.class);
+        productoContratadoMapper = Mockito.mock(ProductoContratadoMapper.class);
+        personaService = new PersonaServiceImpl(direccionMapper,personaMapper,telefonoMapper,tipoViaMapper,provinciaMapper,paisMapper,productoContratadoMapper);
+    }
+
+    @Test
+    public void addPersona_should_throwRequiredMissingFieldException_when_nombreIsNull(){
+        Assertions.assertThrows(Exception.class,() ->{
+            Persona persona = new Persona();
+
+            persona.setNombre(null);
+            this.personaService.addPersona(persona);
+        });
+    }
+
+
+}
