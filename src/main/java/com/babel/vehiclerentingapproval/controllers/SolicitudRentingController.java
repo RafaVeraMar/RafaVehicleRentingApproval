3155d7c68cfa7abce880f7a25b2a8054c685ce9f
Ramon Vazquez <ramon.vazquez@babelgroup.com>
2023-03-31 11:24:40 +0200

    Hecha la documentacion de swagger

--- src/main/java/com/babel/vehiclerentingapproval/controllers/SolicitudRentingController.java
+++ src/main/java/com/babel/vehiclerentingapproval/controllers/SolicitudRentingController.java
@@ -2,13 +2,23 @@ package com.babel.vehiclerentingapproval.controllers;
 
 
 import com.babel.vehiclerentingapproval.exceptions.EstadoSolicitudNotFoundException;
+import com.babel.vehiclerentingapproval.exceptions.SolicitudRentingNotFoundException;
 import com.babel.vehiclerentingapproval.models.SolicitudRenting;
 import com.babel.vehiclerentingapproval.models.TipoResultadoSolicitud;
 import com.babel.vehiclerentingapproval.persistance.database.mappers.SolicitudRentingMapper;
 import com.babel.vehiclerentingapproval.services.SolicitudRentingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/solicitud")
 public class SolicitudRentingController {
@@ -24,32 +34,51 @@ public class SolicitudRentingController {
     @GetMapping("/estado/{id}")
     ResponseEntity<String> verEstadoSolicitud(@PathVariable String id) throws EstadoSolicitudNotFoundException {
 
-        try{
+        try {
             int idSolicitud = Integer.parseInt(id);
             String estado = this.solicitud.verEstadoSolicitud(idSolicitud);
             return ResponseEntity.ok(estado);
-        }
-        catch (EstadoSolicitudNotFoundException e){
+        } catch (EstadoSolicitudNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: No existe ninguna codigo de resolución válido asociado a esa solicitud ");
 
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: Debe de pasar un número como id ");
         }
     }
+
     @GetMapping("{id}")
     ResponseEntity<SolicitudRenting> muestraSolicitudPorId(@PathVariable int id) {
         return ResponseEntity.ok(this.solicitud.getSolicitudById(id));
     }
+
+    @Operation(summary = "Ver estado de solicitud por ID", description = "Devuelve el estado de una solicitud a partir de su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cancelada la solicitud", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Error con el id al cancelar solicitud", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "ID de solicitud no encontrado", content = @Content(mediaType = "application/json"))
+    })
+    @Parameter(name = "id", description = "ID de la solicitud a cancelar", required = true)
     @PatchMapping("/{id}")
-    ResponseEntity cancelarSolicitud(@PathVariable int id){
+    ResponseEntity cancelarSolicitud(@PathVariable int id) {
+        Map<String, Object> respuesta = new HashMap<String, Object>();
         try {
             this.solicitud.cancelarSolicitud(id);
-        }catch(Exception e){//TODO cuando haga el merge meter la excepcion creada por Alavaro
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("no es correcto el id");
+            respuesta.put("Status", HttpStatus.OK);
+            respuesta.put("Id", id);
+            respuesta.put("Descripcion", "Solicitud cancelada");
+            return new ResponseEntity<Object>(respuesta, HttpStatus.OK);
+        } catch (SolicitudRentingNotFoundException e) {
+            respuesta.put("Status", HttpStatus.NOT_FOUND);
+            respuesta.put("Id", id);
+            respuesta.put("Descripcion", "El id de solicitud no es válido");
+            return new ResponseEntity<Object>(respuesta, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            respuesta.put("Status", HttpStatus.BAD_REQUEST);
+            respuesta.put("Id", id);
+            respuesta.put("Descripcion", "Error: No ha introducido una id valida ");
+            return new ResponseEntity<Object>(respuesta, HttpStatus.BAD_REQUEST);
         }
-
-        return ResponseEntity.ok(String.format("Solicitud cancelada id: %d",id));
     }
 }
+
 
