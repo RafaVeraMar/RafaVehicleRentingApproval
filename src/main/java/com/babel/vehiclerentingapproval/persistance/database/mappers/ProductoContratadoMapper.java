597331140321d875920c43a08466e02de3697ffb
Tomas Prados <tomas.prados@babelgroup.com>
2023-03-31 09:29:44 +0200

    ProductoContratadoMapper actualizado

--- src/main/java/com/babel/vehiclerentingapproval/persistance/database/mappers/ProductoContratadoMapper.java
+++ src/main/java/com/babel/vehiclerentingapproval/persistance/database/mappers/ProductoContratadoMapper.java
@@ -8,8 +8,7 @@ import org.apache.ibatis.annotations.Options;
 @Mapper
 public interface ProductoContratadoMapper {
     @Insert("INSERT INTO PRODUCTO_CONTRATADO (PRODUCTO_ID, ALIAS, IMPORTE_NOMINAL, FECHA_ALTA, FECHA_BAJA)" +
-            " VALUES (#{idProducto}, #{alias, jdbcType=VARCHAR}, #{importeNominal}, " +
-            "#{fechaAlta, jdbcType=DATE}, null)")
+            " VALUES (#{idProducto}, #{alias}, #{importeNominal}, #{fechaAlta}, #{fechaBaja, jdbcType=DATE})")
     @Options(useGeneratedKeys = true, keyProperty = "idProductoContratado", keyColumn = "PRODUCTO_CONTRATADO_ID")
     void insertProductoContratado(ProductoContratado productoContratado);
 }
