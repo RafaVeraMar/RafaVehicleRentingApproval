f49dc8b506ddff9e42d3067eb1e853be748b4037
RafaVera <rafael.vera@babelgroup.com>
2023-03-31 13:01:28 +0200

    Test ApprovalRules

--- src/test/java/com/babel/vehiclerentingapproval/services/impl/approvalRulesService.java
+++ src/test/java/com/babel/vehiclerentingapproval/services/impl/approvalRulesService.java
@@ -14,6 +14,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
