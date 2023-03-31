8522fa6b79747a598f3d20ff358fe7d5a17fccb1
RafaVera <rafael.vera@babelgroup.com>
2023-03-31 12:49:53 +0200

    Nacionalidad = española

---
+++ src/test/java/com/babel/vehiclerentingapproval/services/impl/preAutomaticResultService.java
@@ -0,0 +1,9 @@
+package com.babel.vehiclerentingapproval.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class preAutomaticResultService {
+}
+
+@Test
+public void calculateAutomaticResult()
