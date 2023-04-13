a1d88a7c7f49aef644deab86bda7e0f0400e43f4
Javier Serrano <javier.serrano@babelgroup.com>
2023-04-13 13:04:33 +0200

    Documentar excepciones

--- src/main/java/com/babel/vehiclerentingapproval/exceptions/EstadoSolicitudInvalidException.java
+++ src/main/java/com/babel/vehiclerentingapproval/exceptions/EstadoSolicitudInvalidException.java
@@ -2,6 +2,11 @@ package com.babel.vehiclerentingapproval.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * Esta excepción se lanza cuando el codigo de resolución no es válido
+ * @see RequestApiValidationException
+ * @author javier.serrano@babelgroup.com
+ */
 public class EstadoSolicitudInvalidException extends RequestApiValidationException{
     private static final String externalMessage = "El codigo de resolución no es válido";
     private static final HttpStatus statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
--- src/main/java/com/babel/vehiclerentingapproval/exceptions/EstadoSolicitudNotFoundException.java
+++ src/main/java/com/babel/vehiclerentingapproval/exceptions/EstadoSolicitudNotFoundException.java
@@ -2,6 +2,11 @@ package com.babel.vehiclerentingapproval.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * Esta excepción se lanza cuando el codigo de resolución es nulo
+ * @see RequestApiValidationException
+ * @author javier.serrano@babelgroup.com
+ */
 public class EstadoSolicitudNotFoundException extends RequestApiValidationException{
     private static final String externalMessage = "No existe ninguna codigo de resolución asociado a esa solicitud";
     private static final HttpStatus statusCode = HttpStatus.NOT_FOUND;
