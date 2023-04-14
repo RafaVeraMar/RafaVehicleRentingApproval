bfb269aba5d3829928cce56575ede805a9682694
Alvaro Dorado <alvaro.dorado@babelgroup.com>
2023-04-14 12:05:53 +0200

    Documentacion modelo tipoVia
    Documentacion modelo tipoVia

--- src/main/java/com/babel/vehiclerentingapproval/models/TipoVia.java
+++ src/main/java/com/babel/vehiclerentingapproval/models/TipoVia.java
@@ -3,31 +3,67 @@ package com.babel.vehiclerentingapproval.models;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+/**
+ * Este clase modela el tipo de via
+ *
+ * @author tomas.prados@babelgroup.com
+ * @author javier.roldan@babelgroup.com
+ * @author enrique.munoz@babelgroup.com
+
+ */
 public class TipoVia {
+    /**
+     * El identificador del tipo de via
+     */
     @NotNull
     private Integer tipoViaId;
+    /**
+     * La descripcion que define el tipo de via
+     */
     @NotNull
     @Size(max = 50)
     private String descripcion;
 
+    /**
+     * Constructor de la clase {TipoVia}
+     * @param tipoViaId
+     * @param descripcion
+     */
 
     public TipoVia(Integer tipoViaId, String descripcion) {
         this.tipoViaId = tipoViaId;
         this.descripcion = descripcion;
     }
 
+    /**
+     * Metodo publico que devuelve el identificador del tipo de via
+     * @return devuelve el identificador delk tipo de via
+     */
     public Integer getTipoViaId() {
         return tipoViaId;
     }
 
+    /**
+     * metodo publico que inicializa el tipo de via
+     * @param tipoViaId
+     */
     public void setTipoViaId(Integer tipoViaId) {
         this.tipoViaId = tipoViaId;
     }
+
+    /**
+     * metodo publico que devuelve la descripcion del tipo de via
+     * @return
+     */
 
     public String getDescripcion() {
         return descripcion;
     }
 
+    /**
+     * metodo publico que inicializa la descripcion
+     * @param descripcion
+     */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
