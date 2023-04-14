4b391a0a1c0d37b9fd90a8f362a3204ebabfb6e6
Alvaro Dorado <alvaro.dorado@babelgroup.com>
2023-04-14 12:27:58 +0200

    Documentacion Mapper Provincia

--- src/main/java/com/babel/vehiclerentingapproval/persistance/database/mappers/ProvinciaMapper.java
+++ src/main/java/com/babel/vehiclerentingapproval/persistance/database/mappers/ProvinciaMapper.java
@@ -7,12 +7,26 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
+/**
+ * Esta interfaz define un mapper que realiza las operaciones relacionadas con la provincia de la base de datos
+ * @author enrique.munoz@babelgroup.com
+ */
+
 @Mapper
 public interface ProvinciaMapper {
+    /**
+     * Este metodo inserta una provincia en la base de datos
+     * @param provincia la provincia a insertar
+     */
     @Insert("INSERT INTO PROVINCIA (COD_PROVINCIA, NOMBRE) VALUES (#{codProvincia}, #{nombre})")
     @Options(useGeneratedKeys = true, keyProperty = "codProvincia", keyColumn = "COD_PROVINCIA")
     void insertProvincia(Provincia provincia);
 
+    /**
+     * Este metodo realiza un select sobre la base de datos para obtener la provincia
+     * @param codProvincia
+     * @return
+     */
     @Select("SELECT COD_PROVINCIA, NOMBRE FROM PROVINCIA WHERE COD_PROVINCIA = #{codProvincia}")
     Provincia getProvincia(String codProvincia);
 }
