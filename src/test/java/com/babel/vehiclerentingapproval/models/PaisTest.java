6f215d0cc7a634e927a4c75a575054321714f6f0
andresguijarro-babel <andres.guijarro@babelgroup.com>
2023-04-17 12:21:52 +0200

    paistest

--- src/test/java/com/babel/vehiclerentingapproval/models/PaisTest.java
+++ src/test/java/com/babel/vehiclerentingapproval/models/PaisTest.java
@@ -1,2 +1,33 @@
-package com.babel.vehiclerentingapproval.models;public class PaisTest {
+package com.babel.vehiclerentingapproval.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaisTest {
+    Pais pais;
+
+    @BeforeEach
+    void setupAll() {
+
+    }
+
+    @Test
+    public void pais_shouldReturn_sameString() {
+        Pais pais = createPais();
+        Assertions.assertEquals(pais.toString(), "Pais{isoAlfa_2='1', isoNum_3=2, isoAlfa_3='ES', nombre='España', orden=4}");
+    }
+
+    public Pais createPais() {
+        this.pais = new Pais();
+        pais.setIsoAlfa_2("1");
+        pais.setIsoNum_3(2);
+        pais.setIsoAlfa_3("ES");
+        pais.setNombre("España");
+        pais.setOrden(4);
+
+        return pais;
+    }
 }
