f0bd44194dc3d589d5b3815dfd9624be7edc970c
Ismael Mesa <ismael.mesa@babelgroup.com>
2023-04-17 13:39:31 +0200

    Smell2

--- src/main/java/com/babel/vehiclerentingapproval/services/preautomaticresults/impl/CalculateAutomaticResultImpl.java
+++ src/main/java/com/babel/vehiclerentingapproval/services/preautomaticresults/impl/CalculateAutomaticResultImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 /**
  * Clase que contiene los metodos que engloba todos los metodos destinados a comprobar el cumplimiento de reglas de preparobacion,
  * predenegacion, y la obtencion de un estado de la solicitud.
+ *
  * @author: ismael.mesa@babelgroup.com
  * @author: alvaro.dorado@babelgroup.com
  */
@@ -21,8 +22,9 @@ public class CalculateAutomaticResultImpl implements CalculateAutomaticResult {
 
     /**
      * Constructor encargado de dar forma al objeto de tipo CalculateAutomaticResult
-     * @param denyRulesService parámetro de tipo DenyRulesService que contiene todos las reglas de predenegación
-     * @param approvalRulesService parámetro de tipo  ApprovalRulesService que contiene todos las reglas de preaprovación
+     *
+     * @param denyRulesService      parámetro de tipo DenyRulesService que contiene todos las reglas de predenegación
+     * @param approvalRulesService  parámetro de tipo  ApprovalRulesService que contiene todos las reglas de preaprovación
      * @param automaticResultMapper parámetro de tipo AutomaticResultMapper realiza una query que actualiza el código de resolución
      *                              de una solicitud de renting.
      */
@@ -33,6 +35,7 @@ public class CalculateAutomaticResultImpl implements CalculateAutomaticResult {
 
     /**
      * Metodo encargado de comprobar que no se cumpla alguna regla de predenegación
+     *
      * @param solicitudRenting la solicitud de renting
      * @return Si no ha encontrado ninguna regla de predenación devuelve True, en caso contrario False.
      */
@@ -51,23 +54,25 @@ public class CalculateAutomaticResultImpl implements CalculateAutomaticResult {
 
     /**
      * Metodo encargado de comprobar que todas las reglas de preaprobación de cumplan
+     *
      * @param solicitudRenting la solicitud de renting
      * @return Si todas las reglas de preaprobación se cumplen, devuelve True, en caso contrario False.
      */
     @Override
     public Boolean findAllApproval(SolicitudRenting solicitudRenting) {
         boolean resultado;
-        if (approvalRulesService.validateCIFCliente(solicitudRenting) &&
-                approvalRulesService.validateInversion(solicitudRenting) &&
-                approvalRulesService.validateInversionIngresos(solicitudRenting) &&
-                approvalRulesService.validateScoringPersona(solicitudRenting) &&
-                approvalRulesService.validateImpagoCuota(solicitudRenting) &&
-                approvalRulesService.validateNationality(solicitudRenting) &&
-                approvalRulesService.validateYearsExperience(solicitudRenting) &&
-                approvalRulesService.validateClienteNoAprobadoConGarantias(solicitudRenting) &&
-                approvalRulesService.validateClienteNoRechazadoPreviamente(solicitudRenting)) {
+        if (Boolean.TRUE.equals(approvalRulesService.validateCIFCliente(solicitudRenting)) &&
+                Boolean.TRUE.equals(approvalRulesService.validateInversion(solicitudRenting)) &&
+                        Boolean.TRUE.equals(approvalRulesService.validateInversionIngresos(solicitudRenting)) &&
+                                Boolean.TRUE.equals(approvalRulesService.validateScoringPersona(solicitudRenting)) &&
+                                        Boolean.TRUE.equals(approvalRulesService.validateImpagoCuota(solicitudRenting)) &&
+                                                Boolean.TRUE.equals(approvalRulesService.validateNationality(solicitudRenting)) &&
+                                                        Boolean.TRUE.equals(approvalRulesService.validateYearsExperience(solicitudRenting)) &&
+                                                                Boolean.TRUE.equals(approvalRulesService.validateClienteNoAprobadoConGarantias(solicitudRenting)) &&
+                                                                        Boolean.TRUE.equals(approvalRulesService.validateClienteNoRechazadoPreviamente(solicitudRenting)))
+        {
             resultado = true;
-        } else {
+        } else{
             resultado = false;
         }
         return resultado;
@@ -75,21 +80,22 @@ public class CalculateAutomaticResultImpl implements CalculateAutomaticResult {
 
     /**
      * Metodo encargado de comprobar que se cumpla alguna regla de preaprobación
+     *
      * @param solicitudRenting solicitud de renting
      * @return Si alguna regla de preaprobación se cumple devuelve True, en caso contrario, devuelve False
      */
     @Override
     public Boolean findAnyApproval(SolicitudRenting solicitudRenting) {
         boolean resultado;
-        if (this.approvalRulesService.validateCIFCliente(solicitudRenting) ||
-                this.approvalRulesService.validateInversion(solicitudRenting) ||
-                this.approvalRulesService.validateInversionIngresos(solicitudRenting) ||
-                this.approvalRulesService.validateScoringPersona(solicitudRenting) ||
-                this.approvalRulesService.validateImpagoCuota(solicitudRenting) ||
-                this.approvalRulesService.validateNationality(solicitudRenting) ||
-                this.approvalRulesService.validateYearsExperience(solicitudRenting) ||
-                this.approvalRulesService.validateClienteNoAprobadoConGarantias(solicitudRenting) ||
-                this.approvalRulesService.validateClienteNoRechazadoPreviamente(solicitudRenting)) {
+        if (Boolean.TRUE.equals(this.approvalRulesService.validateCIFCliente(solicitudRenting)) ||
+                Boolean.TRUE.equals(this.approvalRulesService.validateInversion(solicitudRenting)) ||
+                Boolean.TRUE.equals(this.approvalRulesService.validateInversionIngresos(solicitudRenting)) ||
+                Boolean.TRUE.equals(this.approvalRulesService.validateScoringPersona(solicitudRenting)) ||
+                Boolean.TRUE.equals(this.approvalRulesService.validateImpagoCuota(solicitudRenting)) ||
+                Boolean.TRUE.equals(this.approvalRulesService.validateNationality(solicitudRenting)) ||
+                Boolean.TRUE.equals(this.approvalRulesService.validateYearsExperience(solicitudRenting)) ||
+                Boolean.TRUE.equals(this.approvalRulesService.validateClienteNoAprobadoConGarantias(solicitudRenting)) ||
+                Boolean.TRUE.equals(this.approvalRulesService.validateClienteNoRechazadoPreviamente(solicitudRenting))) {
             resultado = true;
         } else {
             resultado = false;
@@ -103,6 +109,7 @@ public class CalculateAutomaticResultImpl implements CalculateAutomaticResult {
      * Alguna regla de predenegación se cumple
      * Alguna regla de predenegación se cumple
      * devolviendo el estado de la solicitud, respecto a ello.
+     *
      * @param solicitudRenting la solicitud de renting.
      * @return resultado equivalente al estado de la solicitud tras comprobar todas las validaciones.
      * devolviendo: AA = Aprobada, PA = Pendiente Aprobacion, DM = Predenegacion Automatica.
@@ -127,7 +134,6 @@ public class CalculateAutomaticResultImpl implements CalculateAutomaticResult {
         }
         return result;
     }
-
 
 
 }
