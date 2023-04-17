c48ee3cad2f2b7cb5b0c365340974690f84ee6dd
Javier Serrano <javier.serrano@babelgroup.com>
2023-04-17 12:15:24 +0200

    Eliminados code smells
    -Unused import
    -Usar campoDescripcion en todo el controlador

--- src/main/java/com/babel/vehiclerentingapproval/persistance/database/mappers/TipoResultadoSolicitudMapper.java
+++ src/main/java/com/babel/vehiclerentingapproval/persistance/database/mappers/TipoResultadoSolicitudMapper.java
@@ -3,7 +3,6 @@ package com.babel.vehiclerentingapproval.persistance.database.mappers;
 import com.babel.vehiclerentingapproval.models.TipoResultadoSolicitud;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
