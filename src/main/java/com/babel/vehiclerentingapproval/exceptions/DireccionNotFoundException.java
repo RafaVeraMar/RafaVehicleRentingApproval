0f3a4ecb81c5b423628f0bf740247939c1ea85ca
miguel.sdela <miguel.sdela@babelgroup.com>
2023-04-21 10:12:44 +0200

    Cambios completados y arreglados

--- src/main/java/com/babel/vehiclerentingapproval/exceptions/DireccionNotFoundException.java
+++ src/main/java/com/babel/vehiclerentingapproval/exceptions/DireccionNotFoundException.java
@@ -4,9 +4,11 @@ package com.babel.vehiclerentingapproval.exceptions;
  * Esta clase excepción es un esquema vacío básico para ser lanzado desde los puntos
  * requeridos de la aplicación. Su lanzamiento es resultado de la validacion de los datos
  * de la base de datos. Extiende de la clase Exception
- * @see Exception
  *
  * @author andres.guijarro@babelgroup.com
+ * @see Exception
  */
 public class DireccionNotFoundException extends Exception {
+
+
 }
