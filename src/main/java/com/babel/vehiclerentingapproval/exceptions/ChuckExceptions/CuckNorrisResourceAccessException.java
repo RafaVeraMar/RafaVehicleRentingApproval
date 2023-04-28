5728ae24b2d8b7d003b87d0ed994acad357b26cf
Javier Roldan <javier.roldan@babelgroup.com>
2023-04-28 12:33:15 +0200

    x2

--- src/main/java/com/babel/vehiclerentingapproval/exceptions/ChuckExceptions/CuckNorrisResourceAccessException.java
+++ src/main/java/com/babel/vehiclerentingapproval/exceptions/ChuckExceptions/CuckNorrisResourceAccessException.java
@@ -1,4 +1,12 @@
 package com.babel.vehiclerentingapproval.exceptions.ChuckExceptions;
 
-public class CuckNorrisResourceAccessException {
+public class CuckNorrisResourceAccessException extends ChuckNorrisException{
+    private static final String EXTERNAL_MESSAGE = "Error de servidor en la API externa: handshake failed";
+
+    public CuckNorrisResourceAccessException(String msg) {
+        super(msg);
+    }
+    public CuckNorrisResourceAccessException() {
+        super(EXTERNAL_MESSAGE);
+    }
 }
