6f1d430978190ad01fc3db58a7fa3b4abd69810c
RafaVera <rafael.vera@babelgroup.com>
2023-05-05 10:22:13 +0200

    Puliendo Security JWT Gestionando Excepciones

--- src/main/java/com/babel/vehiclerentingapproval/exceptions/PersonByEmailNotFoundException.java
+++ src/main/java/com/babel/vehiclerentingapproval/exceptions/PersonByEmailNotFoundException.java
@@ -1,2 +1,12 @@
-package com.babel.vehiclerentingapproval.exceptions;public class PersonByEmailNotFoundException {
+package com.babel.vehiclerentingapproval.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class PersonByEmailNotFoundException extends ApplicationException {
+    private static final String EXTERNAL_MESSAGE = "No se ha encontrado ninguna persona con el email %s";
+
+    public PersonByEmailNotFoundException (HttpStatus statusCode, String email){
+        super(EXTERNAL_MESSAGE, statusCode);
+    }
+
 }
